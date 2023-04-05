bd48f435ee3fd57a3ba6a207d8977d7f319e4cbc
Ziyi Yan <ziyi_yan@sfu.ca>
2023-04-05 02:40:28 +0000

    add bw-tree wrapper

---
+++ index-benchmarks/wrappers/bwtree_wrapper.cpp
@@ -0,0 +1,98 @@
+#include <cstdint>
+#include <vector>
+
+#include "indexes/BwTree/bwtree.h"
+
+#include "bwtree_wrapper.hpp"
+
+extern "C" tree_api *create_tree(const tree_options_t &opt) { return new bwtree_wrapper(opt); }
+
+bwtree_wrapper::bwtree_wrapper(const tree_options_t &opt) {
+  tree = new BwTreeType{};
+  tree->UpdateThreadLocal(opt.num_threads);
+}
+
+bwtree_wrapper::~bwtree_wrapper() { delete tree; }
+
+bool bwtree_wrapper::bulk_load(const char *data, size_t num_records, size_t key_sz,
+                                 size_t value_sz) {
+  // Fake bulk loading
+  const char *pos = data;
+  for (uint64_t i = 0; i < num_records; ++i) {
+    uint64_t ikey = *reinterpret_cast<const uint64_t *>(pos);
+    ikey = __builtin_bswap64(ikey);
+    pos += key_sz;
+    uint64_t ival = 0;
+    memcpy(&ival, pos, sizeof(uint64_t));
+    pos += value_sz;
+    bool ok = tree->Insert(ikey, ival);
+    if (!ok) {
+      return false;
+    }
+  }
+  return true;
+}
+
+bool bwtree_wrapper::find(const char *key, size_t key_sz, char *value_out) {
+  uint64_t ikey = *reinterpret_cast<const uint64_t *>(key);
+  ikey = __builtin_bswap64(ikey);
+  std::vector<uint64_t> values;
+  values.reserve(1);
+  tree->GetValue(ikey, values);
+  if (values.size() == 0) {
+    return false;
+  }
+  *reinterpret_cast<uint64_t *>(value_out) = values.at(0);
+  return true;
+}
+
+bool bwtree_wrapper::insert(const char *key, size_t key_sz, const char *value, size_t value_sz) {
+  uint64_t ikey = *reinterpret_cast<const uint64_t *>(key);
+  ikey = __builtin_bswap64(ikey);
+  uint64_t ival = 0;
+  memcpy(&ival, value, sizeof(uint64_t));
+  return tree->Insert(ikey, ival);
+}
+
+bool bwtree_wrapper::update(const char *key, size_t key_sz, const char *value, size_t value_sz) {
+  uint64_t ikey = *reinterpret_cast<const uint64_t *>(key);
+  ikey = __builtin_bswap64(ikey);
+  uint64_t ival = 0;
+  memcpy(&ival, value, sizeof(uint64_t));
+  return tree->Upsert(ikey, ival);
+}
+
+bool bwtree_wrapper::remove(const char *key, size_t key_sz) {
+  uint64_t ikey = *reinterpret_cast<const uint64_t *>(key);
+  ikey = __builtin_bswap64(ikey);
+  uint64_t old_val;
+  return tree->Delete(ikey, old_val);
+}
+
+int bwtree_wrapper::scan(const char *key, size_t key_sz, int scan_sz, char *&values_out) {
+  static thread_local uint64_t buffer[1 << 16];
+  values_out = reinterpret_cast<char *>(buffer);
+  uint64_t ikey = *reinterpret_cast<const uint64_t *>(key);
+  ikey = __builtin_bswap64(ikey);
+  auto it = tree->Begin(ikey);
+  int values_idx = 0;
+  for (; values_idx < scan_sz && !it.IsEnd(); values_idx++, it++) {
+    buffer[values_idx] = it->second;
+  }
+  return values_idx;
+}
+
+thread_local static int my_thread_id;
+
+void bwtree_wrapper::tls_setup() {
+  my_thread_id = next_thread_id.fetch_add(1);
+  tree->AssignGCID(my_thread_id);
+}
+
+void bwtree_wrapper::tls_cleanup() {
+  tree->UnregisterThread(my_thread_id);
+}
+
+void bwtree_wrapper::tls_reset() {
+  next_thread_id = 0;
+}
---
+++ index-benchmarks/wrappers/bwtree_wrapper.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <cstdint>
+
+#include "indexes/BwTree/bwtree.h"
+
+#include "tree_api.hpp"
+
+class bwtree_wrapper : public tree_api {
+public:
+  bwtree_wrapper(const tree_options_t &opt);
+  virtual ~bwtree_wrapper();
+
+  virtual bool bulk_load(const char *data, size_t num_records, size_t key_sz,
+                         size_t value_sz) override final;
+  virtual bool find(const char *key, size_t key_sz, char *value_out) override final;
+  virtual bool insert(const char *key, size_t key_sz, const char *value,
+                      size_t value_sz) override final;
+  virtual bool update(const char *key, size_t key_sz, const char *value,
+                      size_t value_sz) override final;
+  virtual bool remove(const char *key, size_t key_sz) override final;
+  virtual int scan(const char *key, size_t key_sz, int scan_sz, char *&values_out) override final;
+  virtual void tls_setup() override final;
+  void tls_cleanup();
+  void tls_reset();
+
+private:
+  using BwTreeType = wangziqi2013::bwtree::BwTree<uint64_t, uint64_t>;
+  BwTreeType *tree;
+  std::atomic<int> next_thread_id{0};
+};
